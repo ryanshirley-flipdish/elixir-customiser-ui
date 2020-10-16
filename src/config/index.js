019d3ffb3dbbaacf790f9221cf90d8412d0f6c0c
Ryan Shirley <ryan.shirley@flipdish.ie>
2020-10-16 16:20:09 +0100

    Add config with elixir templates custom code

---
+++ src/config/index.js
@@ -0,0 +1,10 @@
+export default {
+    /**
+     * Elixir Templates - CSS & JS
+     */
+    elixirs: {
+        cafeshirley: {
+            css: 'https://cdn.jsdelivr.net/gh/ryanshirley-flipdish/elixir-templates@development/cafeshirley/production.min.css'
+        }
+    },
+}
