aee91c701d2552f0d7f7416df559f5584969d353
Ryan Shirley <ryan.shirley@flipdish.ie>
2020-10-22 13:32:24 +0100

    Add additional templates

--- src/config/index.js
+++ src/config/index.js
@@ -4,8 +4,8 @@ export default {
      */
     elixirs: {
         cafeshirley: {
-            label: 'CafeShirley',
-            css: 'https://cdn.jsdelivr.net/gh/ryanshirley-flipdish/elixir-templates@development/cafeshirley/production.min.css',
+            label: 'Cafe Shirley',
+            css: 'https://cdn.jsdelivr.net/gh/ryanshirley-flipdish/elixir-templates@development/dist/cafeShirley/production.min.css',
             settings: {
                 colours: [
                     {
@@ -47,6 +47,112 @@ export default {
                     {
                         name: 'Body Inverse',
                         variable: 'body-font-color-inverse',
+                        forceRGB: false,
+                        description: 'This changes X.',
+                    },
+                ]
+            }
+        },
+        pizzacafe: {
+            label: 'Pizza Cafe',
+            css: 'https://cdn.jsdelivr.net/gh/ryanshirley-flipdish/elixir-templates@development/dist/pizzaCafe/production.min.css',
+            settings: {
+                colours: [
+                    {
+                        name: 'Main Background',
+                        variable: 'main-bg-color',
+                        forceRGB: false,
+                        description: 'This changes X.',
+                    },
+                    {
+                        name: 'Second Background',
+                        variable: 'second-bg-color',
+                        forceRGB: false,
+                        description: 'This changes X.',
+                    },
+                    {
+                        name: 'Third Background',
+                        variable: 'third-bg-color',
+                        forceRGB: false,
+                        description: 'This changes X.',
+                    },
+                    {
+                        name: 'Headings',
+                        variable: 'heading-font-color',
+                        forceRGB: false,
+                        description: 'This changes X.',
+                    },
+                    {
+                        name: 'Body',
+                        variable: 'body-font-color',
+                        forceRGB: false,
+                        description: 'This changes X.',
+                    },
+                    {
+                        name: 'Secondary Font',
+                        variable: 'secondary-font-color',
+                        forceRGB: false,
+                        description: 'This changes X.',
+                    },
+                    {
+                        name: 'Headings Inverse',
+                        variable: 'heading-font-color-inverse',
+                        forceRGB: false,
+                        description: 'This changes X.',
+                    },
+                    {
+                        name: 'Body Inverse',
+                        variable: 'body-font-color-inverse',
+                        forceRGB: false,
+                        description: 'This changes X.',
+                    },
+                    {
+                        name: 'Secondary Font Inverse',
+                        variable: 'secondary-font-color-inverse',
+                        forceRGB: false,
+                        description: 'This changes X.',
+                    },
+                ]
+            }
+        },
+        lospolloshermanos: {
+            label: 'Los Pollos Hermanos',
+            css: 'https://cdn.jsdelivr.net/gh/ryanshirley-flipdish/elixir-templates@development/dist/losPollosHermanos/production.min.css',
+            settings: {
+                colours: [
+                    {
+                        name: 'Primary',
+                        variable: 'primary-color',
+                        forceRGB: true,
+                        description: 'This changes X.',
+                    },
+                    {
+                        name: 'Accent',
+                        variable: 'accent-color',
+                        forceRGB: true,
+                        description: 'This changes X.',
+                    },
+                    {
+                        name: 'Alt Accent',
+                        variable: 'alt-accent-color',
+                        forceRGB: true,
+                        description: 'This changes X.',
+                    },
+                    {
+                        name: 'Alt Background',
+                        variable: 'atl-bg',
+                        forceRGB: false,
+                        description: 'This changes X.',
+                    },
+                    {
+                        name: 'Highlight',
+                        variable: 'highlight',
+                        forceRGB: false,
+                        description: 'This changes X.',
+                    },
+                    {
+                        name: 'Shadow',
+                        variable: 'shadow',
                         forceRGB: false,
                         description: 'This changes X.',
                     },
