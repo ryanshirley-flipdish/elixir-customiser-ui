2a8d757b0c17f8e881a184630d08365da692bd36
Ryan Shirley <ryan.shirley@flipdish.ie>
2020-10-22 13:16:30 +0100

    Format templates with settings

--- src/config/index.js
+++ src/config/index.js
@@ -4,7 +4,54 @@ export default {
      */
     elixirs: {
         cafeshirley: {
-            css: 'https://cdn.jsdelivr.net/gh/ryanshirley-flipdish/elixir-templates@development/cafeshirley/production.min.css'
+            label: 'CafeShirley',
+            css: 'https://cdn.jsdelivr.net/gh/ryanshirley-flipdish/elixir-templates@development/cafeshirley/production.min.css',
+            settings: {
+                colours: [
+                    {
+                        name: 'Main Background',
+                        variable: 'main-bg-color',
+                        forceRGB: false,
+                        description: 'This changes X.',
+                    },
+                    {
+                        name: 'Main Background Alternate',
+                        variable: 'main-bg-color=alternate',
+                        forceRGB: false,
+                        description: 'This changes X.',
+                    },
+                    {
+                        name: 'Second Background',
+                        variable: 'second-bg-color',
+                        forceRGB: false,
+                        description: 'This changes X.',
+                    },
+                    {
+                        name: 'Headings',
+                        variable: 'heading-font-color',
+                        forceRGB: false,
+                        description: 'This changes X.',
+                    },
+                    {
+                        name: 'Body',
+                        variable: 'body-font-color',
+                        forceRGB: false,
+                        description: 'This changes X.',
+                    },
+                    {
+                        name: 'Headings Inverse',
+                        variable: 'heading-font-color-inverse',
+                        forceRGB: false,
+                        description: 'This changes X.',
+                    },
+                    {
+                        name: 'Body Inverse',
+                        variable: 'body-font-color-inverse',
+                        forceRGB: false,
+                        description: 'This changes X.',
+                    },
+                ]
+            }
         }
     },
 }
