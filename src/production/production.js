aac095405f51f810a914834caf61078f05b86dec
Ryan Shirley <ryan.shirley@flipdish.ie>
2020-10-28 17:06:06 +0000

    Update production.js

--- src/production/production.js
+++ src/production/production.js
@@ -1,6 +1,7 @@
+// Main App file to placing the customiser into elixir page
 import React from "react"
 import ReactDOM from "react-dom"
 
-import Customiser from "../components/customiser"
+import Customiser from "../components/Customiser"
 
 ReactDOM.render(<Customiser />, document.getElementById("fd_customiser"))
